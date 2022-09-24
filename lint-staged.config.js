0283488a2b9a847529410d43acff4a5c5ed8162e
realbaotrung <realbaotrung@gmail.com>
2022-09-24 13:00:44 +0700

    feat: modify lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,4 +2,7 @@ module.exports = {
   'src/**/*.{ts,json,css,html}': [
     'prettier --write --config .prettierrc.js --ignore-path .prettierignore',
   ],
+  'src/*': [
+    'eslint --color -c .eslintrc.js --ext .ts .',
+  ],
 };
